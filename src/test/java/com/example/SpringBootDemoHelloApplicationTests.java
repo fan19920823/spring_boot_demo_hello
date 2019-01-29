b91abf8220c3a8a175a0d40a340ff2fa2f03f9a3
admin <fan504811235@163.com>
2019-01-29 14:51:58 +0800

    使用docker部署springboot

--- src/test/java/com/example/SpringBootDemoHelloApplicationTests.java
+++ src/test/java/com/example/SpringBootDemoHelloApplicationTests.java
@@ -8,9 +8,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDemoHelloApplicationTests {
-
+	public static void main(String[] args){
+		float s=0f;
+        System.out.println(s==0);
+	}
 	@Test
 	public void contextLoads() {
+//		String codeType = ((CodeType)Enum.valueOf(CodeType.class, "ServiceI")).getValue();
+        System.out.println("123");
 	}
+	public static enum CodeType
+	{
+		serviceImpl("ServiceImpl"),
+		service("ServiceI"), controller("Controller"), entity("Entity"), jsp(""), jspList("List");
 
+		private String type;
+
+		private CodeType(String type) { this.type = type; }
+
+		public String getValue()
+		{
+			return this.type;
+		}
+	}
 }
